16ba14c86015873192f8100e868f7133e05bdb08
Nicolas Garcia <playchathac@gmail.com>
2026-02-27 11:29:32 +0100

    reset game on end

--- server/src/controllers/resetGame.ts
+++ server/src/controllers/resetGame.ts
@@ -1,3 +1,13 @@
 import Game from "../models/Game";
 
-export const resetGame = async (gameId: string) => {}
+export const resetGame = async (gameId: string) => {
+    try {
+        const game = await Game.findOne({ gameId });
+        if (!game) throw new Error('Game not found');
+
+
+    } catch (error) {        
+        console.error('Error resetting game:', error);
+        throw error;
+    }
+}
